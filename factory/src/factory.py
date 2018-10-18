20509f989a83ffb171b5e8eec255a5be38fd845d
alimsanzhar <sanzhar.alim19@gmail.com>
2018-10-18 18:14:34 +0600

    feat: add factory class

---
+++ factory/src/factory.py
@@ -0,0 +1,23 @@
+"""
+Factory Design Pattern.
+"""
+
+class Company(object):
+    def factory(type):
+        if type == "Google": 
+            return Google()
+        if type == "Apple":
+            return Apple()
+        TypeError("Bad company creation: " + type)
+    factory = staticmethod(factory)
+
+class Google(Company):
+    def invent(self): 
+        print("Invent Google Glass")
+
+class Apple(Company): 
+    def invent(self):
+        print("Invent iPhone")
+
+object = Company.factory("Google")
+object.invent()
