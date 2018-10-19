e5d2663ac1b5cc38f430da4468ce2a86513e5560
An4ik <akhmetov.anuar09@gmail.com>
2018-10-19 10:57:17 +0600

    some notices

--- factory/src/factory.py
+++ factory/src/factory.py
@@ -2,22 +2,29 @@
 Factory Design Pattern.
 """
 
+
 class Company(object):
+
     def factory(type):
-        if type == "Google": 
+        if type == "Google":
             return Google()
         if type == "Apple":
             return Apple()
-        TypeError("Bad company creation: " + type)
+        raise TypeError("Bad company creation: " + type)
+
     factory = staticmethod(factory)
 
+
 class Google(Company):
-    def invent(self): 
+
+    def invent(self):
         print("Invent Google Glass")
 
-class Apple(Company): 
+
+class Apple(Company):
     def invent(self):
         print("Invent iPhone")
 
-object = Company.factory("Google")
+
+object = Company.factory("G+ASFoogle")
 object.invent()
